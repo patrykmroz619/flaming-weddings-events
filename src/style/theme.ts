a9b5ae4eaa17292c669d3721b5e202ac9b2d41a4
Patryk Mróz <patrykmroz619@wp.pl>
2021-04-19 22:44:42 +0200

    feat: add styled components and create the theme object

---
+++ src/style/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primary: `#c21500`,
+    font: `#222222`,
+  },
+  spacing: {
+    xs: `5px`,
+    s: `10px`,
+    m: `20px`,
+    l: `50px`,
+    xl: `100px`,
+  },
+
+  gradient: `linear-gradient(to right, #c21500 0%, #ffc500  51%, #c21500  100%)`,
+
+  contentWidth: `1200px`,
+} as const;
+
+export type ThemeType = typeof theme;
