49daa248567672dbaec3123ee6608143199a2769
Patryk Mróz <patrykmroz619@wp.pl>
2021-04-19 23:02:57 +0200

    feat(components): create Button component

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: block;
+  padding: ${({ theme: { spacing } }) => `0 ${spacing.l}`};
+  height: 40px;
+  line-height: 40px;
+
+  color: #eee;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 14px;
+
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+
+  transition: 0.5s;
+  background-size: 200% auto;
+  background-image: ${({ theme }) => theme.gradient};
+
+  &:hover {
+    background-position: right center;
+  }
+`;
