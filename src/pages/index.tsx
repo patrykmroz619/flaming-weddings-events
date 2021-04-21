52acba9265410ebe3218cedce8e2b3299f4bcc14
Patryk Mróz <patrykmroz619@wp.pl>
2021-04-21 11:49:13 +0200

    feat(home page): add AboutSection prototype

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { MainSection } from '@/components/HomePage';
+import { AboutSection, MainSection } from '@/components/HomePage';
 import Layout from '@/components/Layout';
 
 export const Home = () => (
   <Layout>
     <MainSection />
+    <AboutSection />
   </Layout>
 );
 
--- src/style/Global.ts
+++ src/style/Global.ts
@@ -4,6 +4,7 @@ import { breakpoints, minWidth } from './media';
 const GlobalStyle = createGlobalStyle`
   html {
     --content-x-margin: ${({ theme }) => theme.spacing.s};
+    --content-y-margin: ${({ theme }) => theme.spacing.s};
 
     ${minWidth(breakpoints.s)} {
       --content-x-margin: ${({ theme }) => theme.spacing.m};
@@ -18,12 +19,12 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Lato", 'arial', sans-serif;
   }
 
   body {
     width: 100vw;
     overflow-x: hidden;
+    font-family: "Lato", 'arial', sans-serif;
     font-size: 16px;
     color: ${({ theme }) => theme.colors.font};
 
@@ -41,6 +42,23 @@ const GlobalStyle = createGlobalStyle`
   h1, h2, h3 {
     font-family: 'Bangers';
     letter-spacing: 3px;
+    text-shadow: 3px 3px 6px #888;
+  }
+
+  h2 {
+    font-size: 30px;
+
+    ${minWidth(breakpoints.m)} {
+      font-size: 40px;
+    }
+
+    ${minWidth(breakpoints.l)} {
+      font-size: 55px;
+    }
+
+    ${minWidth(breakpoints.xl)} {
+      font-size: 65px;
+    }
   }
 
   ul {
