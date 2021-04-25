cdea22ce7b1490fddc4ebac01ffd6a6218be9061
Patryk Mróz <patrykmroz619@wp.pl>
2021-04-25 17:35:35 +0200

    style(components): add underline to the Heading component

--- src/components/common/Heading/index.ts
+++ src/components/common/Heading/index.ts
@@ -1,6 +1,20 @@
 import styled from 'styled-components';
 
 export const Heading = styled.h2`
-  padding: ${({ theme }) => theme.spacing.m} 0 ${({ theme }) => theme.spacing.s};
+  position: relative;
+  padding: ${({ theme }) => theme.spacing.m} 0 ${({ theme }) => theme.spacing.m};
   text-align: center;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 70px;
+    height: 5px;
+    background-image: ${({ theme }) => theme.gradient};
+    background-size: 200%;
+    border-radius: 5px;
+  }
 `;
